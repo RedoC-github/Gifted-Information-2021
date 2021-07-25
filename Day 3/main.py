4ef2758af437d014cf055b40f8cebb34bea727df
RedoC-github <coderlovemedia@gmail.com>
2021-07-26 01:01:51 +0900

    Day 4

--- Day 3/main.py
+++ Day 3/main.py
@@ -2,6 +2,7 @@ ans = [1849, 272400, 35]
 
 import pandas as pd
 import random as rd
+from ml import LogRegression
 
 ds = pd.read_csv('ds.csv')
 data = [ds['2'].tolist(), ds['3'].tolist(), ds['4'].tolist()]
@@ -25,14 +26,27 @@ print(allavg)
 import matplotlib.pyplot as plt
 
 quest1 = [e[0] for e in allavg]
+mx = max(quest1)
+mn = min(quest1)
+quest1 = [(e-mn)/(mx-mn) for e in quest1]
 quest2 = [e[1] for e in allavg]
+mx = max(quest2)
+mn = min(quest2)
+quest2 = [(e-mn)/(mx-mn) for e in quest2]
 quest3 = [e[2] for e in allavg]
+mx = max(quest3)
+mn = min(quest3)
+quest3 = [(e-mn)/(mx-mn) for e in quest3]
 
 plt.plot(range(1, n+1, 1), quest1)
 plt.show()
+LogRegression(range(1, n+1, 1), quest1)
+
 
 plt.plot(range(1, n+1, 1), quest2)
 plt.show()
+LogRegression(range(1, n+1, 1), quest2)
 
 plt.plot(range(1, n+1, 1), quest3)
 plt.show()
+LogRegression(range(1, n+1, 1), quest3)
