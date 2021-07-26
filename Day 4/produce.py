50fa464cd51f01afc3810cdc5c535b55ba2d5cb1
RedoC <59589012+RedoC-github@users.noreply.github.com>
2021-07-26 11:38:53 +0900

    Update produce.py

--- Day 4/produce.py
+++ Day 4/produce.py
@@ -11,6 +11,7 @@ for i in range(2, n+1, 1):
 		for j in range(2*i, n+1, i):
 			arr[j] = 0
 
+# Regression
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.pyplot as plt
@@ -19,19 +20,19 @@ import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
 
 plt.title("Prime Distribution")
-plt.scatter(np.arange(0, len(primes)/2, 0.5), primes)
+plt.scatter(np.arange(len(primes)), primes)
 plt.ylabel("n-th prime")
 plt.xlabel("n")
 
 # regression
 
-learning_rate = 0.000001
-epoch = 40
+learning_rate = 0.001
+epoch = 4000
 
 X = tf.placeholder(tf.float32)
 Y = tf.placeholder(tf.float32)
 
-deg = 2
+deg = 4
 def model(X_, w):
 	polynoms = []
 	for i in range(deg):
@@ -45,18 +46,19 @@ y_model = model(X, w)
 cost = (tf.pow(Y-y_model, 2))
 train_op = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
 
-with tf.Session() as sess:
-	init = tf.global_variables_initializer()
-	sess.run(init)
+sess = tf.Session()
+init = tf.global_variables_initializer()
+sess.run(init)
 
-	for epc in range(epoch):
-		for (x, y) in zip(np.arange(0, len(primes)/2, 0.5), primes):
-			sess.run(train_op, feed_dict={X: x, Y: y})
-	
-	w_val = sess.run(w)
-	print(w_val)
+for epc in range(epoch):
+  for (x, y) in zip(np.arange(len(primes)), primes):
+    sess.run(train_op, feed_dict={X: x, Y: y})
 
-poly_X = np.arange(0, len(primes)/2, 0.5)
+w_val = sess.run(w)
+print(w_val)
+sess.close()
+
+poly_X = np.arange(len(primes))
 poly_Y = []
 for x_ in poly_X:
 	poly_y = 0
